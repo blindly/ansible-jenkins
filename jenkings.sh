27f2808b4cd6bc60982af07c249c3c8d70ce8e0d
blindly <blindly@users.noreply.github.com>
2018-08-16 20:59:26 -0400

    Create jenkings.sh

---
+++ jenkings.sh
@@ -0,0 +1,12 @@
+export DOCKER_HOST="tcp://127.0.0.1:2375"
+
+CONTAINER=$(docker run -d -v "$WORKSPACE:/build:rw" ansible-ubuntu /bin/bash -c 'cd /build && bash ./test.sh isilon')
+
+echo "CONTAINER: $CONTAINER"
+docker attach $CONTAINER
+
+RC=$(docker wait $CONTAINER)
+echo "RC: $RC"
+
+docker rm $CONTAINER
+exit $RC
