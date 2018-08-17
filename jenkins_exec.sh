f57a72a60ba27e8f6ce06d96174c7cc94e2b9009
blindly <blindly@users.noreply.github.com>
2018-08-17 06:46:41 -0400

    Update jenkins_exec.sh

--- jenkins_exec.sh
+++ jenkins_exec.sh
@@ -1,6 +1,6 @@
 export DOCKER_HOST="tcp://127.0.0.1:2375"
 
-CONTAINER=$(docker run -d -v "$WORKSPACE:/build:rw" ansible-ubuntu /bin/bash -c 'cd /build && bash ./test.sh isilon')
+CONTAINER=$(docker run --rm -d -v "$WORKSPACE:/build:rw" ansible-ubuntu /bin/bash -c 'cd /build && bash ./test.sh <directory name>')
 
 echo "CONTAINER: $CONTAINER"
 docker attach $CONTAINER
